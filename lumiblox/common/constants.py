7aa1420ff5c3521b2b9912b0e65b9b87dedfbfeb
lherrman <herrmann.lars@gmx.ch>
2026-02-18 10:45:50 +0100

    refactor: update GUI scene column constant for consistency with Launchpad layout

--- lumiblox/common/constants.py
+++ lumiblox/common/constants.py
@@ -4,5 +4,5 @@
 ROWS_PER_PAGE = 5
 NUM_SCENE_PAGES = 2
 SCENE_COLUMNS = 9   # Total columns on Launchpad (including right-side control column)
-GUI_SCENE_COLUMNS = 8  # Columns shown in the GUI (excluding right-side control column)
+GUI_SCENE_COLUMNS = 9  # Columns shown in the GUI scene grid
 TOTAL_SCENE_ROWS = ROWS_PER_PAGE * NUM_SCENE_PAGES
